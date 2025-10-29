9fd15648f191365b5bb9d16b235003bc2829c677
Anton <465692@niuitmo.ru>
2025-10-29 12:03:49 +0300

    add tg mentor-bot f

---
+++ mentor_bot/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN can't be found. Add token to .env")
+
+print("TOKEN UPLOADED")
